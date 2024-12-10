8a1b9b5aa3d6ec94a2ee0c8aa40eca9f912ad4e9
mRcOol7 <196450307014@kjpbharuch.gujgov.edu.in>
2024-12-10 17:46:43 +0530

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Home from "./(routes)/home/page";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function main() {
   return (
       <div>
+        <Analytics/>
           <Home />
       </div>
   );
