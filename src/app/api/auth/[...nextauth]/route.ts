6ad8da81ad20c799b88e74e80b50e6c21782df7e
mRcOol7 <196450307014@kjpbharuch.gujgov.edu.in>
2024-12-10 17:03:53 +0530

    ..

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,9 +2,8 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 import TwitterProvider from "next-auth/providers/twitter";
-import { AuthOptions } from "next-auth";
 
-export const authOptions: AuthOptions = {
+const handler = NextAuth({
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -84,7 +83,6 @@ export const authOptions: AuthOptions = {
             return token;
         }
     }
-};
+});
 
-const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
