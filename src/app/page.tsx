daf5a71e03ea608060f82a62d4c15705dc090c42
mRcOol7 <196450307014@kjpbharuch.gujgov.edu.in>
2024-12-18 12:44:51 +0530

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Home from "./(routes)/home/page";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function main() {
   return (
       <div>
         <Analytics/>
+        <SpeedInsights />
           <Home />
       </div>
   );
