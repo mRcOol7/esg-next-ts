6acb358da5f1340bd42f5d0f32addae03ba01655
mRcOol7 <196450307014@kjpbharuch.gujgov.edu.in>
2024-12-18 16:08:06 +0530

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Home from "./(routes)/home/page";
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 export default function main() {
   return (
